d0ee30c8c074f369f3e464a74114cc10807ce9fc
Daamin909 <ashai.daamin@gmail.com>
2024-10-24 17:58:54 +0530

    Interm UI Design Complete

--- chessbro/src/components/ChessboardComponent/ChessboardComponent.jsx
+++ chessbro/src/components/ChessboardComponent/ChessboardComponent.jsx
@@ -8,14 +8,16 @@ const ChessboardComponent = () => {
     const board = Chessboard(boardRef.current, {
       position: "start",
       draggable: false,
+      innerWidth: "12vw",
       pieceTheme:
         "https://chessboardjs.com/img/chesspieces/wikipedia/{piece}.png",
     });
 
     return () => board.destroy();
   }, []);
-
+  window.addEventListener("resize", () => {
+    document.getElementById("chessboard");
+  });
   return <div id="chessboard" ref={boardRef} />;
 };
-
 export default ChessboardComponent;
--- chessbro/src/components/ReviewPanel/ReviewPanel.jsx
+++ chessbro/src/components/ReviewPanel/ReviewPanel.jsx
@@ -1,21 +1,20 @@
 import React from "react";
 import "./ReviewPanel.css";
-import rook from "../../assets/branding/logo.png";
 const ReviewPanel = () => {
   return (
     <div id="review-panel">
       <div id="review-panel-header">
-        <img id="rook" src={rook} />
-        <h2>Analyse</h2>
+        <h2>Game Review</h2>
       </div>
+      <hr />
       <div id="game-input-container">
         <form id="game-input-form">
           <div id="game-input-box">
-            <textarea
+            <input
               type="text"
               id="game-input"
               placeholder="Enter PGN here"
-            ></textarea>
+            ></input>
             <select id="game-input-type">
               <option value="pgn">PGN</option>
             </select>
