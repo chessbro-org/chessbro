d368aa9e8fe09950dfb6a691a99fc2414cdacadf
Daamin909 <ashai.daamin@gmail.com>
2024-10-27 15:15:28 +0530

    Added: Stockfish

--- backend/gamereview.py
+++ backend/gamereview.py
@@ -3,19 +3,20 @@ import chess.pgn
 import io
 import requests
 import json
-def analyse(FENs):
+from stockfish import Stockfish
+def getEngineAnalysis(FENs):
     response = []
-    x=0
+    x = 0
     for FEN in FENs:
-        x+=1
-        API_ENDPOINT = "https://chess-api.com/v1"
-        data = {'fen': FEN}
-        r = requests.post(url=API_ENDPOINT, data=data)
+        API_ENDPOINT = "https://stockfish.online/api/s/v2.php"
+        data = {'fen': FEN, 'depth': 10}
+        r = requests.get(url=API_ENDPOINT, params=data)
         response.append(r.json())
-        if (x==10):
-            break
         print(f'{x} is done')
+        x+=1
     return response
+
+
 def review_game (pgn):
     game = chess.pgn.read_game(io.StringIO(pgn))
     board = game.board()
@@ -24,27 +25,35 @@ def review_game (pgn):
     for move in moves:
         board.push(move)
         moves_fens.append(board.fen())
-    review = analyse(moves_fens)
+    review = getEngineAnalysis(moves_fens)
     with open("response.json", "w", encoding="utf-8") as f:
         json.dump(review, f, ensure_ascii=False, indent=4)
 
 review_game("""[Event "Live Chess"]
 [Site "Chess.com"]
-[Date "2024.10.20"]
+[Date "2024.09.25"]
 [Round "?"]
-[White "mido_sigma"]
-[Black "daamin_101"]
-[Result "0-1"]
+[White "daamin_101"]
+[Black "arturfraga"]
+[Result "1-0"]
 [TimeControl "600"]
-[WhiteElo "800"]
-[BlackElo "1224"]
+[WhiteElo "1257"]
+[BlackElo "1211"]
 [Termination "daamin_101 won by checkmate"]
-[ECO "B01"]
-[EndTime "12:30:03 GMT+0000"]
-[Link "https://www.chess.com/game/live/123166182575"]
+[ECO "D02"]
+[EndTime "11:38:28 GMT+0000"]
+[Link "https://www.chess.com/game/live/121002057375"]
 
-1. e4 d5 2. exd5 Qxd5 3. Nf3 Nc6 4. Nc3 Qa5 5. b4 Qxb4 6. Rb1 Qa5 7. Bd3 e5 8.
-O-O Bg4 9. Qe1 Bxf3 10. gxf3 Nf6 11. f4 Bb4 12. fxe5 Nxe5 13. Rxb4 Qxb4 14. Bb2
-Qa5 15. Ne4 Nxe4 16. Bxe4 O-O 17. Bxb7 Rab8 18. Bc3 Qb6 19. Bf3 Nxf3+ 20. Kg2
-Nxe1+ 21. Rxe1 Qg6+ 22. Kf3 Qf5+ 23. Kg2 Qg4+ 24. Kf1 Rfe8 25. Rxe8+ Rxe8 26.
-Bd4 Rb8 27. Bc3 Rb1# 0-1""")
+1. d4 d5 2. Bf4 Nf6 3. e3 g6 4. Nf3 Bg7 5. c4 c6 6. Nc3 O-O 7. Be2 Be6 8. O-O
+Nbd7 9. Qb3 Nb6 10. c5 Nc4 11. Qxb7 Qc8 12. Qxc8 Rfxc8 13. b3 Na5 14. Ne5 Ne4
+15. Nxe4 dxe4 16. Ba6 Rd8 17. b4 Bxe5 18. Bxe5 Nc4 19. Bg3 Bd5 20. Rac1 Na3 21.
+Rc3 Nb5 22. Bxb5 cxb5 23. Ra1 e6 24. a4 a6 25. a5 Bc4 26. Bc7 Rdc8 27. Bd6 Rc6
+28. f3 f5 29. fxe4 fxe4 30. Ra2 Rd8 31. Rf2 Rd7 32. Rf8+ Kg7 33. Re8 Rf7 34.
+Be5+ Kh6 35. Rc1 Kg5 36. Bf4+ Kg4 37. h3+ Kf5 38. g4+ Kf6 39. g5+ Kf5 40. Kg2 h5
+41. gxh6 g5 42. Be5 Rh7 43. Rf8+ Kg6 44. Rf6+ Kh5 45. h4 gxh4 46. Rh1 Rxh6 47.
+Rxh6+ Kxh6 48. Rxh4+ Kg5 49. Rxe4 Bd5 50. Kf3 Bxe4+ 51. Kxe4 Kg4 52. d5 exd5+
+53. Kxd5 Rg6 54. e4 Kf3 55. Bd6 Rg5+ 56. e5 Ke2 57. c6 Rg4 58. c7 Rc4 59. Bc5
+Kd3 60. c8=Q Kc3 61. Bd4+ Kb3 62. Qxc4+ bxc4 63. e6 Kxb4 64. e7 Kxa5 65. e8=Q
+Kb4 66. Qb8+ Ka3 67. Qb2+ Ka4 68. Bc3 a5 69. Qc2+ Kb5 70. Qb2+ Ka4 71. Qa2+ Kb5
+72. Qxc4+ Kb6 73. Qc6+ Ka7 74. Bd4+ Kb8 75. Be5+ Ka7 76. Qc7+ Ka6 77. Kc5 a4 78.
+Qb6# 1-0""")
