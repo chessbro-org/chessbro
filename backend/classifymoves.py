45770122d9cb144f4b8df2174a381993e6722f11
daamin909 <ashai.daamin@gmail.com>
2024-10-31 21:51:39 +0530

    Added: Book Moves | Move Grading | Backend Completed

--- backend/classifymoves.py
+++ backend/classifymoves.py
@@ -1,8 +1,39 @@
 import math
+import json
+import chess
 
+def isBookMove(fen):
+    board = chess.Board()
+    board.set_fen(fen)
+    fen = board.board_fen()
+    with open("assets/openings/openings.json", "r") as file:
+        openings = json.load(file)
+    for opening in openings:
+        if (opening['fen']==fen):
+            return [True, opening['name']]
+    return [False]
+
+def gradeMove(eval_change):
+    if (eval_change>=4.5):
+        return ("blunder")
+    elif (eval_change>=2.6):
+        return ("mistake")
+    elif (eval_change>=1.3):
+        return ("inaccuracy")
+    elif (eval_change>=0.8):
+        return ("good")
+    elif (eval_change>=0.1):
+        return ("excellent")
+    else:
+        return ("best_move")
+    
+def isBestMove(move_info):
+    return (move_info['best_move'] ==  move_info['move'])
 
 def classifyMoves(analysis):
     evalDiffs= [None]
+    openings = ["Starting Position"]
+    
     for counter in range(1, len(analysis)): 
 
         current = analysis[counter]
@@ -10,26 +41,39 @@ def classifyMoves(analysis):
 
         current_type = current['eval']['type']
         previous_type = previous['eval']['type']
+    
+        book_move = isBookMove(current['fen'])
 
-        if (current_type=='cp' and previous_type=='cp'):
-            resp = cp_and_cp(current, previous)
-            evalDiffs.append(resp)
-        elif (current_type=='cp' and previous_type=='mate'):
-            resp = cp_and_mate(current, previous)
-            evalDiffs.append(resp)
-        elif (current_type=='mate' and previous_type=='mate'):
-            resp = mate_and_mate(current, previous)
-            evalDiffs.append(resp)
-        elif (current_type=='mate' and previous_type=='cp'):
-            resp = mate_and_cp(current, previous)
-            evalDiffs.append(resp)
+        if (book_move[0]):
+            evalDiffs.append('book_move')
+            openings.append(book_move[1])
+        else: 
+            openings.append(None)
+            if (isBestMove(current)):
+                evalDiffs.append('best_move')
+            elif (current_type=='cp' and previous_type=='cp'):
+                resp = cp_and_cp(current, previous)
+                evalDiffs.append(resp)
+            elif (current_type=='cp' and previous_type=='mate'):
+                resp = cp_and_mate(current, previous)
+                evalDiffs.append(resp)
+            elif (current_type=='mate' and previous_type=='mate'):
+                resp = mate_and_mate(current, previous)
+                evalDiffs.append(resp)
+            elif (current_type=='mate' and previous_type=='cp'):
+                resp = mate_and_cp(current, previous)
+                evalDiffs.append(resp)
 
-    return evalDiffs
+    for counter, (move_type, opening) in enumerate(zip(evalDiffs, openings)):
+        analysis[counter]['opening'] = opening
+        analysis[counter]['move_type'] = move_type
+
+    return analysis
     
 def cp_and_cp(current, previous):
     diff = previous['eval']['value'] - current['eval']['value']
     diff = math.floor(diff*100)/100
-    return abs(diff)
+    return gradeMove(abs(diff)) 
 
 def cp_and_mate(current, previous):
     # TODO replace the move name    
@@ -37,9 +81,11 @@ def cp_and_mate(current, previous):
     current_eval = current['eval']['value']
     if (current_eval >= 20):
         return ("excellent")
-    elif (current_eval >= 10):
+    elif (current_eval >= 12):
         return ("good")
-    elif (current_eval >= 5):
+    elif (current_eval >= 9):
+        return ("inaccuracy")
+    elif (current_eval>=6):
         return ("mistake")
     else:
         return ("blunder")    
@@ -50,7 +96,7 @@ def mate_and_cp(current, previous):
         return ("good")
     elif (previous_eval >= 20):
         return ("inaccuracy")
-    elif (previous_eval >= 7):
+    elif (previous_eval >= 10):
         return ("mistake")
     else:
         return ("blunder")
--- backend/gamereview.py
+++ backend/gamereview.py
@@ -1,20 +1,25 @@
 import chess
-import chess.pgn 
 import io
-import json
 from stockfish import Stockfish
 from classifymoves import classifyMoves
 
-stockfish = Stockfish(path=r"/usr/games/stockfish", depth=16, parameters={"Threads": 1, "Minimum Thinking Time": 1, "Hash": 32, "Slow Mover": 10})
+stockfish = Stockfish(path=r"/usr/games/stockfish", depth=16, parameters={"Threads": 1, "Minimum Thinking Time": 1, "Hash": 512, "Slow Mover": 30})
 
 def getEngineAnalysis(FENs):
     response = []
     for count, FEN in enumerate(FENs):
+
+        if (count == 0):
+            bestmove = False
+        else:
+            stockfish.set_fen_position(FENs[count-1])
+            bestmove = stockfish.get_best_move()   
+
         stockfish.set_fen_position(FEN)
-        bestmove = stockfish.get_best_move()
         eval = stockfish.get_evaluation()
         compiled = {'move_no': count, 'fen': FEN, 'best_move': bestmove, 'eval': eval}
         response.append(compiled)
+        print(count)
 
     return response
 
@@ -22,14 +27,14 @@ def changeFormat(pgn, infos, moves):
     game = chess.pgn.read_game(io.StringIO(pgn))
     board = game.board()
     default_fen = board.fen()
-    for (info, move) in zip(infos, moves):
+    for counter, (info, move) in enumerate(zip(infos, moves)):
 
         if (info['eval']['type']!='mate'):
             info['eval']['value']/=100
         if (not info['best_move']):
             info['best_move'] = (None)
         else:
-            board.set_fen(info['fen'])
+            board.set_fen(infos[counter-1]['fen'])
             info['best_move'] = (board.san(chess.Move.from_uci(info['best_move'])))
             board.set_fen(default_fen)    
         info['move'] = move
@@ -49,40 +54,5 @@ def review_game (pgn):
     analysis = getEngineAnalysis(moves_fens)
     analysis = changeFormat(pgn, analysis, moves_san)
     analysis = classifyMoves(analysis)
- 
-def getEngineAnalysis_testing(m):
-    with open("withoutmoves.json", "r") as file:
-         review = json.load(file)
-    return review
 
-
-
-review_game("""[Event "Live Chess"]
-[Site "Chess.com"]
-[Date "2024.09.25"]
-[Round "?"]
-[White "daamin_101"]
-[Black "arturfraga"]
-[Result "1-0"]
-[TimeControl "600"]
-[WhiteElo "1257"]
-[BlackElo "1211"]
-[Termination "daamin_101 won by checkmate"]
-[ECO "D02"]
-[EndTime "11:38:28 GMT+0000"]
-[Link "https://www.chess.com/game/live/121002057375"]
-
-1. d4 d5 2. Bf4 Nf6 3. e3 g6 4. Nf3 Bg7 5. c4 c6 6. Nc3 O-O 7. Be2 Be6 8. O-O
-Nbd7 9. Qb3 Nb6 10. c5 Nc4 11. Qxb7 Qc8 12. Qxc8 Rfxc8 13. b3 Na5 14. Ne5 Ne4
-15. Nxe4 dxe4 16. Ba6 Rd8 17. b4 Bxe5 18. Bxe5 Nc4 19. Bg3 Bd5 20. Rac1 Na3 21.
-Rc3 Nb5 22. Bxb5 cxb5 23. Ra1 e6 24. a4 a6 25. a5 Bc4 26. Bc7 Rdc8 27. Bd6 Rc6
-28. f3 f5 29. fxe4 fxe4 30. Ra2 Rd8 31. Rf2 Rd7 32. Rf8+ Kg7 33. Re8 Rf7 34.
-Be5+ Kh6 35. Rc1 Kg5 36. Bf4+ Kg4 37. h3+ Kf5 38. g4+ Kf6 39. g5+ Kf5 40. Kg2 h5
-41. gxh6 g5 42. Be5 Rh7 43. Rf8+ Kg6 44. Rf6+ Kh5 45. h4 gxh4 46. Rh1 Rxh6 47.
-Rxh6+ Kxh6 48. Rxh4+ Kg5 49. Rxe4 Bd5 50. Kf3 Bxe4+ 51. Kxe4 Kg4 52. d5 exd5+
-53. Kxd5 Rg6 54. e4 Kf3 55. Bd6 Rg5+ 56. e5 Ke2 57. c6 Rg4 58. c7 Rc4 59. Bc5
-Kd3 60. c8=Q Kc3 61. Bd4+ Kb3 62. Qxc4+ bxc4 63. e6 Kxb4 64. e7 Kxa5 65. e8=Q
-Kb4 66. Qb8+ Ka3 67. Qb2+ Ka4 68. Bc3 a5 69. Qc2+ Kb5 70. Qb2+ Ka4 71. Qa2+ Kb5
-72. Qxc4+ Kb6 73. Qc6+ Ka7 74. Bd4+ Kb8 75. Be5+ Ka7 76. Qc7+ Ka6 77. Kc5 a4 78.
-Qb6# 1-0
-""")
+    return analysis
