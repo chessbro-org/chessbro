86c51757cf430469cd28c1207031ea3a9fd330a7
Daamin909 <ashai.daamin@gmail.com>
2024-11-04 20:31:14 +0530

    Added: EvalBar functionality | TODO: improve looks

--- chessbro/src/components/EvalBar/EvalBar.jsx
+++ chessbro/src/components/EvalBar/EvalBar.jsx
@@ -1,31 +1,80 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
-import "./EvalBar.css"
+import "./EvalBar.css";
 
-const EvalBar = ({PGN, currentMove, flipped}) => {
-    const [gameEval, setGameEval] = useState(['cp', 0])
+const EvalBar = ({ PGN, currentMove, flipped }) => {
+  const [evalStyle, setEvalStyle] = useState([
+    { height: "50%" },
+    { height: "50%" },
+    "",
+    "",
+  ]);
 
-    useEffect(() => {    
-        if (PGN.move_evaluations[currentMove].eval){
-            const currentEvalType = PGN.move_evaluations[currentMove].eval.type
-            const currentEval = PGN.move_evaluations[currentMove].eval.value
-            setGameEval([currentEvalType, currentEval])
+  useEffect(() => {
+    var x = PGN.move_evaluations[currentMove].eval;
+    if (x) {
+      if (x.type) {
+        const evalType = PGN.move_evaluations[currentMove].eval.type;
+        const evalValue = PGN.move_evaluations[currentMove].eval.value;
+        if (evalType === "mate") {
+          if (evalValue === 0) {
+            const winner = PGN.move_evaluations[currentMove].move_no % 2 === 0;
+            if (winner) {
+              setEvalStyle([{ height: "100%" }, { height: "0" }, `0-1`, ""]);
+            } else {
+              setEvalStyle([{ height: "0" }, { height: "100%" }, "", `1-0`]);
+            }
+          } else if (evalValue > 0) {
+            setEvalStyle([
+              { height: "0" },
+              { height: "100%" },
+              "",
+              `M${Math.abs(evalValue)}`,
+            ]);
+          } else if (evalValue < 0) {
+            setEvalStyle([
+              { height: "100%" },
+              { height: "0" },
+              `M${Math.abs(evalValue)}`,
+              "",
+            ]);
+          }
+        } else if (evalType === "cp") {
+          if (evalValue === 0) {
+            setEvalStyle([{ height: "50%" }, { height: "50%" }, "", ""]);
+          }
+          evalValue > 0
+            ? setEvalStyle([
+                { height: `calc(50% - ${Math.abs(evalValue) * 4}%)` },
+                { height: `calc(50% + ${Math.abs(evalValue) * 4}%)` },
+                "",
+                `${Math.abs(evalValue)}`,
+              ])
+            : setEvalStyle([
+                { height: `calc(50% + ${Math.abs(evalValue) * 4}%)` },
+                { height: `calc(50% - ${Math.abs(evalValue) * 4}%)` },
+                `${Math.abs(evalValue)}`,
+                "",
+              ]);
         }
-    }, [PGN, currentMove, flipped])
+      }
+    }
+  }, [currentMove]);
 
-    const blackStyle = {}
-    const whiteStyle = {}
-    
   return (
-    <div className='eval-bar'>
-        <div className='black'>
-            <a className='eval-value' id='black-eval' style={blackStyle}></a>
-        </div>
-        <div className='white'>            
-            <a className='eval-value' id='black-eval' style={whiteStyle}></a>
-        </div>
+    <div className="eval-bar">
+      <div className="black" style={evalStyle[0]}>
+        <a className="eval-value" id="black-eval">
+          {evalStyle[2]}
+        </a>
+      </div>
+      <div className="white" style={evalStyle[1]}>
+        <a className="eval-value" id="black-eval">
+          {evalStyle[3]}
+        </a>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default EvalBar
+export default EvalBar;
