8c7d8652b9002619b5acef19a72546abb6aa1dea
Daamin909 <ashai.daamin@gmail.com>
2024-11-07 23:06:26 +0530

    fixed: location

--- chessbro/src/components/Accuracy/Accuracy.jsx
+++ chessbro/src/components/Accuracy/Accuracy.jsx
@@ -6,12 +6,12 @@ const Accuracy = ({ PGN }) => {
   const black = PGN.accuracy ? PGN.accuracy.black : 100;
   return (
     <div className="accuracy">
-      <div className="accuracy-color" id="black-accuracy">
-        {`${black}%`}
-      </div>
-      <a id="accuracy-heading">Accuracy</a>
       <div className="accuracy-color" id="white-accuracy">
         {`${white}%`}
+      </div>
+      <a id="accuracy-heading">Accuracy</a>
+      <div className="accuracy-color" id="black-accuracy">
+        {`${black}%`}
       </div>
     </div>
   );
