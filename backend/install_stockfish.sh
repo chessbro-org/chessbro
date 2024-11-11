014a8c1cef7404b5ebce5c051ab23471cca3ae63
Daamin909 <ashai.daamin@gmail.com>
2024-11-11 19:59:06 +0530

    Added: stockfish

--- backend/install_stockfish.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mkdir -p stockfish
-cd stockfish
-curl -L -o stockfish.tar.gz https://github.com/official-stockfish/Stockfish/releases/latest/download/stockfish-ubuntu-x86-64.tar
-tar -xzf stockfish.tar.gz
-chmod +x stockfish
