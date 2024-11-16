1d27ce27a764d4e85ee689d724d5cccffe253790
Daamin909 <ashai.daamin@gmail.com>
2024-11-16 18:12:04 +0530

    added games web-scraping from chess.com

--- backend/chessbro.py
+++ backend/chessbro.py
@@ -1,2 +1,3 @@
 import validation
 import gamereview
+import webscraper
--- backend/server.py
+++ backend/server.py
@@ -18,5 +18,13 @@ def reviewGame():
     reviewed_game = chessbro.gamereview.review_game(pgn)
     return jsonify(reviewed_game)
 
+@app.route('/api/get-chesscom-games', methods=['POST'])
+def getChesscomGames():
+    username = request.get_json()['username']
+    start = request.get_json()['start_date']
+    end = request.get_json()['end_date']
+    games = chessbro.webscraper.getGames(username=username, start_date=start, end_date=end)
+    return jsonify(games)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
