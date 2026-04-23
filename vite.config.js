4e478f79674a04beab47e3c28f63990f87e90f35
Daamin Ashai <ashai.daamin@gmail.com>
2026-04-24 00:55:40 +0530

    Revert "merge shadcn (#6)"
    This reverts commit 90976ba6a014050ad3aa247aa4f2cd5a9c71158a.

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import crossOriginIsolation from "vite-plugin-cross-origin-isolation";
-import tailwindcss from "@tailwindcss/vite";
-import path from "path";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
+  plugins: [react(), 
     // crossOriginIsolation()
   ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
 });
